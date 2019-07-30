62776363c971352b2abdd0c4c09d91b09bf6385c
Laila <laila-pedersen@cuebic.co.jp>
2019-07-30 18:29:19 +0900

    renamed title

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-  res.render('index', { title: 'Express' });
 router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Cache Checker' });
+});
 
 /* POST form to receive sheet with page information */
 router.post('/check_cache', (req, res, next) => {
