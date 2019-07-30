f5a2ca0e7f1f0c7b3680186933ab6029375adc5c
Laila <laila-pedersen@cuebic.co.jp>
2019-07-30 18:29:08 +0900

    cache checker form and endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const router = express.Router();
 /* GET home page. */
   res.render('index', { title: 'Express' });
 router.get('/', (req, res, next) => {
+
+/* POST form to receive sheet with page information */
+router.post('/check_cache', (req, res, next) => {
+  const spreadsheeturl = req.body.spreadsheeturl
+  res.send('Thank you, the result will end up at ' + spreadsheeturl)
+
 });
 
 module.exports = router;
