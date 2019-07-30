02a73dabe7c58d45ec1cf9210cfdcf59f524efe1
Laila <laila-pedersen@cuebic.co.jp>
2019-07-30 18:29:30 +0900

    preparing for controllers

---
+++ controllers/google_spreadsheet.js
@@ -0,0 +1,4 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const googleCacheUrl = 'http://webcache.googleusercontent.com/search?q=cache:'
---
+++ controllers/sonet_cache.js
@@ -0,0 +1,4 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const sonetQueryUrl  = 'https://www.so-net.ne.jp/search/web/?query=site:'
